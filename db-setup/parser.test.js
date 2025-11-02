bf7b84249e333b2ef6b12068ed57725838f59e23
Matthew Kim <mekim0426@gmail.com>
2025-11-02 15:23:36 -0700

    wrote test to compare course count in txt file to courses put into json by parser.ks

---
+++ db-setup/parser.test.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+// Check courses in course.json
+const rawText = fs.readFileSync('./db-setup/course.json', 'utf-8');
+const courses = JSON.parse(rawText);
+
+const courseCount = courses.length;
+
+// Check number of entries in txt.txt
+const txt = fs.readFileSync('./Webscraper 1.0/txt.txt', 'utf-8');
+const lines = txt.split('\n');
+let count = 0;
+
+for (const line of lines) {
+  if (line.trim().startsWith('---')) {
+    count++;
+  }
+}
+count++; // account for first course not having a --- line before it
+
+
+test('Total number of courses parsed correctly', () => {
+  expect(courseCount).toBe(count)// Replace 100 with the expected number of courses
+});
